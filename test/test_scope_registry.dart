b8670663372cdee0c497f6e6648befca29a175fd
alireza easazade <easazade@gmail.com>
2024-08-09 09:12:07 +0330

    Add tests for weaver

---
+++ test/test_scope_registry.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/src/foundation/change_notifier.dart';
+import 'package:weaver/di/base/scope_registry.dart';
+import 'package:weaver/di/base/weaver.dart';
+
+class TestScopeRegistry extends ScopeRegistry {
+  TestScopeRegistry({
+    this.stringObject,
+    this.intObject,
+    this.doubleObject,
+    this.boolObject,
+    this.name = 'test',
+    this.initialIsInScopeValue = true,
+  });
+
+  final String? stringObject;
+  final int? intObject;
+  final double? doubleObject;
+  final bool? boolObject;
+  final bool initialIsInScopeValue;
+
+  @override
+  late ValueNotifier<bool> isInScope = ValueNotifier(initialIsInScopeValue);
+
+  @override
+  Future<void> register(final Weaver weaver) async {
+    if (stringObject != null) {
+      weaver.register(stringObject!);
+    }
+    if (intObject != null) {
+      weaver.register(intObject!);
+    }
+    if (doubleObject != null) {
+      weaver.register(doubleObject!);
+    }
+    if (boolObject != null) {
+      weaver.register(boolObject!);
+    }
+  }
+
+  @override
+  final String name;
+
+  @override
+  Future<void> unregister(final Weaver weaver) async {
+    if (stringObject != null) {
+      weaver.unregister<String>();
+    }
+    if (intObject != null) {
+      weaver.unregister<int>();
+    }
+    if (doubleObject != null) {
+      weaver.unregister<double>();
+    }
+    if (boolObject != null) {
+      weaver.unregister<bool>();
+    }
+  }
+}
--- test/weaver_test.dart
+++ test/weaver_test.dart
@@ -1,12 +1,187 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:weaver/di/base/weaver.dart';
 
-import 'package:weaver/weaver.dart';
+import 'test_scope_registry.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  final weaverInstances = [
+    // default instance
+    weaver,
+    // instantiated instance
+    Weaver(),
+  ];
+
+  for (var i = 0; i < weaverInstances.length; i++) {
+    final weaver = weaverInstances[i];
+    group(
+      'Weaver instance $i - ',
+      () {
+        tearDown(() {
+          weaver.reset();
+          weaver.allowReassignment = false;
+        });
+
+        test(
+          'When no object registered should not be able to fetch it',
+          () {
+            expect(() => weaver.get<String>(), throwsException);
+          },
+        );
+
+        test(
+          'When object registered should be able to fetch it',
+          () {
+            weaver.register('ali');
+            expect(weaver.get<String>(), 'ali');
+          },
+        );
+
+        test(
+          'When object registered should be able to fetch it and '
+          'when unregistered should not be able to fetch it.',
+          () {
+            weaver.register('ali');
+            expect(weaver.get<String>(), 'ali');
+            weaver.unregister<String>();
+            expect(() => weaver.get<String>(), throwsException);
+          },
+        );
+
+        test(
+          'Should be able to request an object from di class before it is created using getAsync method. '
+          'And wait for its creation and fetch the object as soon as it is created.',
+          () async {
+            Future.delayed(const Duration(milliseconds: 500), () {
+              weaver.register('Ali');
+            });
+
+            expect(() => weaver.get<String>(), throwsException);
+            expect(await weaver.getAsync<String>(), 'Ali');
+          },
+        );
+
+        test(
+          'isRegistered method should be able to tell whether a dependency object is registered or not',
+          () {
+            weaver.register('ali');
+            expect(weaver.isRegistered<String>(), true);
+            weaver.unregister<String>();
+            expect(weaver.isRegistered<String>(), false);
+          },
+        );
+
+        test(
+          'When multiple objects registered should be able to fetch each',
+          () {
+            weaver.register('ali');
+            weaver.register<int>(10);
+            weaver.register(true);
+
+            expect(weaver.get<String>(), 'ali');
+            expect(weaver.get<int>(), 10);
+            expect(weaver.get<bool>(), true);
+          },
+        );
+
+        test(
+          'Should not be able to re-register objects of the same type',
+          () {
+            weaver.register('ali');
+            expect(() => weaver.register('till'), throwsException);
+          },
+        );
+
+        test(
+          'Should be able to re-register objects of the same type when allowed reassignment',
+          () {
+            weaver.allowReassignment = true;
+            weaver.register('ali');
+            weaver.register('till');
+
+            expect(weaver.get<String>(), 'till');
+          },
+        );
+
+        test(
+          'When registered a new ScopeRegistry which currently is in scope '
+          'its dependencies should be registered and available to fetch',
+          () {
+            final scopeRegistry = TestScopeRegistry(
+              stringObject: 'ali',
+              intObject: 9,
+            );
+
+            weaver.addScopeRegistry(scopeRegistry);
+
+            expect(weaver.get<String>(), 'ali');
+            expect(weaver.get<int>(), 9);
+          },
+        );
+
+        test(
+          'Should not allow registering RegistryScopes with duplicate names',
+          () {
+            final scopeRegistry = TestScopeRegistry(
+              stringObject: 'ali',
+              intObject: 9,
+              name: 'duplicate-scope-name',
+            );
+
+            weaver.addScopeRegistry(scopeRegistry);
+
+            final scopeRegistry2 = TestScopeRegistry(
+              intObject: 90,
+              name: 'duplicate-scope-name',
+            );
+
+            expect(
+                () => weaver.addScopeRegistry(scopeRegistry2), throwsException);
+          },
+        );
+
+        test(
+          'When removed a ScopeRegistry which currently is in scope '
+          'its dependencies should not be available to fetch anymore',
+          () {
+            final scopeRegistry = TestScopeRegistry(
+              stringObject: 'ali',
+              intObject: 9,
+            );
+
+            weaver.addScopeRegistry(scopeRegistry);
+
+            expect(weaver.get<String>(), 'ali');
+            expect(weaver.get<int>(), 9);
+
+            // remove scopeRegistry
+
+            weaver.removeScopeRegistry(scopeRegistry.name);
+            expect(() => weaver.get<String>(), throwsException);
+            expect(() => weaver.get<int>(), throwsException);
+          },
+        );
+
+        test(
+          'Should register the dependencies of a registered ScopeRegistry after it comes in scope',
+          () {
+            final scopeRegistry = TestScopeRegistry(
+              stringObject: 'ali',
+              intObject: 9,
+              initialIsInScopeValue: false,
+            );
+
+            weaver.addScopeRegistry(scopeRegistry);
+
+            expect(() => weaver.get<String>(), throwsException);
+            expect(() => weaver.get<int>(), throwsException);
+
+            scopeRegistry.isInScope.value = true;
+
+            expect(weaver.get<String>(), 'ali');
+            expect(weaver.get<int>(), 9);
+          },
+        );
+      },
+    );
+  }
 }
