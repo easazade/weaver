98ec14566f5d83368426b88cb10178f4db86a379
Ali <easazade@gmail.com>
2025-08-07 02:28:55 +0330

    Format file

--- lib/src/base/scope.dart
+++ lib/src/base/scope.dart
@@ -36,7 +36,6 @@ abstract class ScopeHandler<T> {
   var scopeHandlerState = ScopeHandlerState.left;
 
   void handle(final Weaver weaver) {
-
     final scope = weaver.scopes
         .firstWhereOrNull((final scope) => scope.name == scopeName);
     final isInScope = scope != null;
