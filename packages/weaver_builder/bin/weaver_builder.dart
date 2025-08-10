b470c4884b879f5dc18826ab8721b1ba09c6daa8
Ali <easazade@gmail.com>
2025-08-10 11:16:56 +0330

    Add example project

--- packages/weaver_builder/bin/weaver_builder.dart
+++
@@ -1,5 +0,0 @@
-import 'package:weaver_builder/weaver_builder.dart' as weaver_builder;
-
-void main(List<String> arguments) {
-  print('Hello world: ${weaver_builder.calculate()}!');
-}
--- packages/weaver_builder/test/weaver_builder_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:weaver_builder/weaver_builder.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
