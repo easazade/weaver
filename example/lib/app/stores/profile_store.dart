fc93dd91a6582a1f4a736f54a6612d3ce2be314b
alireza easazade <easazade@gmail.com>
2026-02-09 12:21:31 +0330

    commit last changes

---
+++ example/lib/app/stores/profile_store.dart
@@ -0,0 +1,2 @@
+//TODO: After shared data feature is available in crystalline store create a ProfileStore
+// that watches the shared user object managed by AuthStore and inside init() it will get the correct profile
